6999f205ebe0d8c5423b0f3527428a4b6f83f087
NemoChenTW <nemochentw@gmail.com>
2016-01-13 13:29:53 +0800

    新增 class FTPCore.

---
+++ src/FTPCore/FTPCore.cpp
@@ -0,0 +1,20 @@
+/*
+ * FTPCore.cpp
+ *
+ *  Created on: 2016年1月13日
+ *      Author: nemo
+ */
+
+#include "FTPCore.h"
+
+FTPCore::FTPCore()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+FTPCore::~FTPCore()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/FTPCore/FTPCore.h
@@ -0,0 +1,17 @@
+/*
+ * FTPCore.h
+ *
+ *  Created on: 2016年1月13日
+ *      Author: nemo
+ */
+
+#ifndef SRC_FTPCORE_FTPCORE_H_
+#define SRC_FTPCORE_FTPCORE_H_
+
+class FTPCore {
+public:
+	FTPCore();
+	virtual ~FTPCore();
+};
+
+#endif /* SRC_FTPCORE_FTPCORE_H_ */
