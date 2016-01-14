5775cde066de6289d5530bb1d0cef4263b67e12b
NemoChenTW <nemochentw@gmail.com>
2016-01-14 17:32:33 +0800

    新增 FTPDef.cpp.

---
+++ src/FTPCore/FTPDef.cpp
@@ -0,0 +1,61 @@
+/*
+ * FTPDef.cpp
+ *
+ *  Created on: 2016年1月14日
+ *      Author: nemochen
+ */
+
+
+#include "FTPDef.h"
+
+namespace FTPDef
+{
+char* str_FTPType[] = {"fileUpload", "folderUpload", "fileDownload", "folderDownload"};
+
+UploadInfo &UploadInfo::operator=(const UploadInfo &right)
+{
+	this->name 				= right.name;
+	this->localPath 		= right.localPath;
+	this->remoteUploadPath 	= right.remoteUploadPath;
+	this->successFolderPath	= right.successFolderPath;
+
+	return (*this);
+}
+
+DownloadInfo &DownloadInfo::operator=(const DownloadInfo &right)
+{
+	this->name 					= right.name;
+	this->remoteDownloadPath 	= right.remoteDownloadPath;
+	this->localPath 			= right.localPath;
+
+	return (*this);
+}
+
+ConnectInfo & ConnectInfo::operator=(const ConnectInfo &right)
+{
+	this->serverIP 		= right.serverIP;
+	this->loginName 	= right.loginName;
+	this->loginPassword = right.loginPassword;
+
+	return (*this);
+}
+
+FTPAction & FTPAction::operator=(const FTPAction &right)
+{
+	this->ftpType 		= right.ftpType;
+	this->connectInfo 	= right.connectInfo;
+	this->ftpInfo		= right.ftpInfo;
+
+	return (*this);
+}
+
+/**
+ * @brief	Show member information.
+ */
+void FTPAction::show()
+{
+	cout << "ftpType = " << str_FTPType[(int)ftpType] << endl;
+	connectInfo.show();
+	ftpInfo->show();
+}
+}
--- src/FTPCore/FTPDef.h
+++ src/FTPCore/FTPDef.h
@@ -21,7 +21,6 @@ using namespace std;
 		fileDownload,
 		folderDownload
 	};
-	char* str_FTPType[] = {"fileUpload", "folderUpload", "fileDownload", "folderDownload"};
 
 	class FTPInfo
 	{
@@ -120,51 +119,9 @@ using namespace std;
 
 		FTPAction & operator=(const FTPAction &right);
 
-		void show()
-		{
-			cout << "ftpType = " 		<< str_FTPType[(int)ftpType] << endl;
-			connectInfo.show();
-			ftpInfo->show();
-		}
+		///	Show member information.
+		void show();
 	};
-
-
-	UploadInfo &UploadInfo::operator=(const UploadInfo &right)
-	{
-		this->name 				= right.name;
-		this->localPath 		= right.localPath;
-		this->remoteUploadPath 	= right.remoteUploadPath;
-		this->successFolderPath	= right.successFolderPath;
-
-		return (*this);
-	}
-
-	DownloadInfo &DownloadInfo::operator=(const DownloadInfo &right)
-	{
-		this->name 					= right.name;
-		this->remoteDownloadPath 	= right.remoteDownloadPath;
-		this->localPath 			= right.localPath;
-
-		return (*this);
-	}
-
-	ConnectInfo & ConnectInfo::operator=(const ConnectInfo &right)
-	{
-		this->serverIP 		= right.serverIP;
-		this->loginName 	= right.loginName;
-		this->loginPassword = right.loginPassword;
-
-		return (*this);
-	}
-
-	FTPAction & FTPAction::operator=(const FTPAction &right)
-	{
-		this->ftpType 		= right.ftpType;
-		this->connectInfo 	= right.connectInfo;
-		this->ftpInfo		= right.ftpInfo;
-
-		return (*this);
-	}
 }
 
 
