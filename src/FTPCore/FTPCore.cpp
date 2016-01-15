435d8c1477e8e17e19e0455b839e51599d97c334
NemoChenTW <nemochentw@gmail.com>
2016-01-15 09:32:20 +0800

    修正 Compile error.

--- src/FTPCore/FTPCore.cpp
+++ src/FTPCore/FTPCore.cpp
@@ -25,7 +25,7 @@ FTPCore::~FTPCore()
  */
 void FTPCore::addFTPAction(FTPAction ftpAction)
 {
-	vector_FTPAction.push_back(ftpAction);
+	list_FTPAction.push_back(ftpAction);
 }
 
 /**
@@ -33,8 +33,8 @@ void FTPCore::addFTPAction(FTPAction ftpAction)
  */
 void FTPCore::processFTPAction()
 {
-	for(auto i = vector_FTPAction.begin();
-			i != vector_FTPAction.end(); i++)
+	for(auto i = list_FTPAction.begin();
+			i != list_FTPAction.end(); i++)
 	{
 		(*i).show();
 	}
