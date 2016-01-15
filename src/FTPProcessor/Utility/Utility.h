238fe5f3962c3fe5fb56c0005b1a1b0845171b98
NemoChenTW <nemochentw@gmail.com>
2016-01-15 10:03:56 +0800

    新增 Utility.h.

---
+++ src/FTPProcessor/Utility/Utility.h
@@ -0,0 +1,15 @@
+/*
+ * Utility.h
+ *
+ *  Created on: 2016年1月15日
+ *      Author: nemochen
+ */
+
+#ifndef SRC_FTPPROCESSOR_UTILITY_UTILITY_H_
+#define SRC_FTPPROCESSOR_UTILITY_UTILITY_H_
+
+
+
+
+
+#endif /* SRC_FTPPROCESSOR_UTILITY_UTILITY_H_ */
