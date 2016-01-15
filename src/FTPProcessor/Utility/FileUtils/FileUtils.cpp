e272ea4705a441303afac388128a08b0b043184a
NemoChenTW <nemochentw@gmail.com>
2016-01-15 10:08:18 +0800

    新增 FileUtils.

---
+++ src/FTPProcessor/Utility/FileUtils/FileUtils.cpp
@@ -0,0 +1,17 @@
+/*
+ * FileUtils.cpp
+ *
+ *  Created on: 2016年1月15日
+ *      Author: nemochen
+ */
+
+#include <FTPProcessor/Utility/FileUtils/FileUtils.h>
+
+namespace FTPProcessor {
+namespace Utility {
+namespace FileUtlis {
+
+
+} /* namespace FileUtlis */
+} /* namespace Utility */
+} /* namespace FTPProcessor */
---
+++ src/FTPProcessor/Utility/FileUtils/FileUtils.h
@@ -0,0 +1,20 @@
+/*
+ * FileUtils.h
+ *
+ *  Created on: 2016年1月15日
+ *      Author: nemochen
+ */
+
+#ifndef SRC_FTPPROCESSOR_UTILITY_FILEUTILS_FILEUTILS_H_
+#define SRC_FTPPROCESSOR_UTILITY_FILEUTILS_FILEUTILS_H_
+
+namespace FTPProcessor {
+namespace Utility {
+namespace FileUtlis {
+
+
+} /* namespace FileUtlis */
+} /* namespace Utility */
+} /* namespace FTPProcessor */
+
+#endif /* SRC_FTPPROCESSOR_UTILITY_FILEUTILS_FILEUTILS_H_ */
