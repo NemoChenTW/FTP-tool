a8ab5f8719f8eb51fa4b16e12a92418347ab9d5f
NemoChenTW <nemochentw@gmail.com>
2016-01-15 11:16:53 +0800

    新增 FileUtils methods.

--- src/FTPProcessor/Utility/FileUtils/FileUtils.cpp
+++ src/FTPProcessor/Utility/FileUtils/FileUtils.cpp
@@ -7,10 +7,110 @@
 
 #include <FTPProcessor/Utility/FileUtils/FileUtils.h>
 
+#include <fstream>
+#include <iostream>
+
 namespace FTPProcessor {
 namespace Utility {
 namespace FileUtlis {
 
+using namespace std;
+	/**
+	 * @brief	Create directory.
+	 *
+	 * @param 	dirPath		Input directory path.
+	 *
+	 * @return	True if directory create successful, false otherwise.
+	 */
+	bool createDir(const std::string dirPath)
+	{
+		if (!fileExist(dirPath.c_str()))
+		{
+			if (mkdir(dirPath.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == -1)
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @brief	Create directories.
+	 *
+	 * @param 	dirPath		Input directories path.
+	 *
+	 * Create directories by using Linux command "mkdir -p".
+	 */
+	void createDirs(const string dirPath)
+	{
+		string cmd = "mkdir -p ";
+		cmd.append(dirPath);
+		system(cmd.c_str());
+	}
+
+	/**
+	 * @brief	Copy file form source to destination.
+	 *
+	 * @param 	srcFilePath		Input source path.
+	 * @param	destFilePath	Input destination path.
+	 *
+	 * @return	True if file copy successful, false otherwise.
+	 */
+	bool copyFile(const char* srcFilePath, const char* destFilePath)
+	{
+		if (!fileExist(srcFilePath))
+			return false;
+
+		std::ifstream src(srcFilePath,  std::ios::binary);
+		std::ofstream dst(destFilePath, std::ios::binary);
+
+		cout << "CopyFile successfully from " << srcFilePath << " ... " << destFilePath << endl;
+
+		dst << src.rdbuf();
+		return true;
+	}
+
+
+	/**
+	 * @brief	Move file form source to destination.
+	 *
+	 * @param	srcFilePath
+	 * @param	destFilePath
+	 *
+	 * @return	True if file move successful, false otherwise.
+	 */
+	bool moveFile(const char* srcFilePath, const char* destFilePath)
+	{
+		if (rename(srcFilePath, destFilePath) == 0)
+		{
+			cout << "Move file successfully from " << srcFilePath << " ... " << destFilePath << endl;
+			return true;
+		}
+		else
+		{
+			cout << "Error move file from " << srcFilePath << " ... " << destFilePath << endl;
+			return false;
+		}
+	}
+
+	/**
+	 * @brief	Remove file.
+	 *
+	 * @param 	filePath	Input file path.
+	 *
+	 * @return	True if file delete successful, false otherwise.
+	 */
+	bool removeFile(const char* filePath)
+	{
+		if (fileExist(filePath) && remove(filePath) != 0)
+		{
+			cout << "Error deleting file " << filePath << endl;
+			return false;
+		}
+		else
+		{
+			cout << "File successfully deleted " << filePath << endl;
+			return true;
+		}
+	}
 
 } /* namespace FileUtlis */
 } /* namespace Utility */
--- src/FTPProcessor/Utility/FileUtils/FileUtils.h
+++ src/FTPProcessor/Utility/FileUtils/FileUtils.h
@@ -8,10 +8,42 @@
 #ifndef SRC_FTPPROCESSOR_UTILITY_FILEUTILS_FILEUTILS_H_
 #define SRC_FTPPROCESSOR_UTILITY_FILEUTILS_FILEUTILS_H_
 
+#include <string>
+#include <sys/stat.h>
+
 namespace FTPProcessor {
 namespace Utility {
 namespace FileUtlis {
 
+	/**
+	 * @brief	Check the file is exist or not.
+	 *
+	 * @param	filePath	Input file path
+	 * .
+	 * @return	True if the file is exist, false otherwise.
+	 */
+	inline bool fileExist(const char* filePath)
+	{
+		struct stat buf;
+		int result = stat(filePath, &buf);
+
+		return (result == 0);
+	}
+
+	///	Create directory.
+    bool createDir(const std::string dirPath);
+
+    ///	Create directories.
+    void createDirs(const std::string dirPath);
+
+    ///	Copy file form source to destination.
+    bool copyFile(const char* srcFilePath, const char* destFilePath);
+
+    ///	Move file form source to destination.
+    bool moveFile(const char* srcFilePath, const char* destFilePath);
+
+    ///	Remove file.
+    bool removeFile(const char* filePath);
 
 } /* namespace FileUtlis */
 } /* namespace Utility */
